e7dfe8a5d726e98eb625e02730582c524dd4cc75
jack <181404551+JackOfNoneTrades@users.noreply.github.com>
2026-03-19 13:42:22 +0100

    updated buildscript and deps

---
+++ addon.gradle.kts
@@ -0,0 +1,10 @@
+tasks.withType<JavaExec>().configureEach {
+    if (name.startsWith("runServer")) {
+        doFirst("wawelauthStripClientOnlyMods") {
+            classpath = classpath.filter { file ->
+                val n = file.name
+                !n.contains("ModularUI2", ignoreCase = true) && !n.contains("Baubles-Expanded", ignoreCase = true) && !n.contains("angelica", ignoreCase = true)
+            }
+        }
+    }
+}
